7ce7cd7e0c56720b946f0ba6891c57532cdde428
Bruno Victor de Mesquita Ferreira <Brunovf1@gmail.com>
2016-01-25 17:34:05 +0000

    fix commit 4fbe1368b3ad83ab16b7384ed426b54a7bb117d8

---
+++ server/methods.js
@@ -0,0 +1,19 @@
+Meteor.methods({
+      'insertPlayerData': function(playerNameVar){
+        var currentUserId = Meteor.userId();
+        PlayersList.insert({
+          name: playerNameVar,
+          score: 0,
+          createdBy: currentUserId,
+        });
+      },
+      'removePlayerData':function(selectedPlayer){
+        var currentUserId = Meteor.userId();
+        PlayersList.remove({_id: selectedPlayer, createdBy: currentUserId});
+      },
+      'modifyPlayerScore': function(selectedPlayer, scoreValue){
+        var currentUserId = Meteor.userId();
+        PlayersList.update({_id: selectedPlayer, createdBy: currentUserId},
+                        {$inc: {score: scoreValue}});
+      },
+});
---
+++ server/publish.js
@@ -0,0 +1,4 @@
+Meteor.publish('thePlayers', function(){
+      var currentUserId = this.userId;
+      return PlayersList.find({createdBy: currentUserId});
+});
